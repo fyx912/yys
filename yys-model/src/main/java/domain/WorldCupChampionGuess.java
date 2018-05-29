d7c08f46a1754408d33ec5a4a91f9055697ddc1d
fyx912 <1314520love>
2018-05-29 15:59:05 +0800

    update

--- yys-model/src/main/java/domain/WorldCupChampionGuess.java
+++ yys-model/src/main/java/domain/WorldCupChampionGuess.java
@@ -22,4 +22,76 @@ public class WorldCupChampionGuess {
     private Date createTime;//创建时间
     private Date updateTime;//更新时间
     private String imgUrl;//图片路径
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getwId() {
+        return wId;
+    }
+
+    public void setwId(Integer wId) {
+        this.wId = wId;
+    }
+
+    public String getBallTeam() {
+        return ballTeam;
+    }
+
+    public void setBallTeam(String ballTeam) {
+        this.ballTeam = ballTeam;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Double getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(Double bonus) {
+        this.bonus = bonus;
+    }
+
+    public String getProbability() {
+        return probability;
+    }
+
+    public void setProbability(String probability) {
+        this.probability = probability;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
 }
